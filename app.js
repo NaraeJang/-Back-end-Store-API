826aee47db981b159809a65cd67ffc7465b8ce91
NaraeJang <j.naaaarae@gmail.com>
2024-01-11 14:44:12 -0500

    install dependencies & basic setup

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('04 Store API')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>home page</h>`);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}...`);
+});
