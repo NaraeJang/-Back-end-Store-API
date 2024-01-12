2fbdeb57fb14cda3ee5ffb791de1cc2130982329
NaraeJang <j.naaaarae@gmail.com>
2024-01-12 16:32:10 -0500

    Setup QueryObject in controllers

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,30 @@
-const getAllProducts = (req, res) => {
-  res.status(200).json({ success: true, msg: "on the way" });
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  // throw new Error("testing async errors");
+  const products = await Product.find({ name: "vase table" });
+  res.status(200).json({ products, nbHit: products.length });
 };
 
-const getAllProductsStatic = (req, res) => {
-  throw new Error("testing async errors");
-  res.status(200).json({ success: true, msg: "Static" });
+const getAllProducts = async (req, res) => {
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (name) {
+    queryObject.name = name;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHit: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
