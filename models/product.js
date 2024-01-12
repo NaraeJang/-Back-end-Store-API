9cf0bdfd07109ee793e7d4157ba1aceb5a0675df
NaraeJang <j.naaaarae@gmail.com>
2024-01-12 15:17:01 -0500

    setup Schema: enum, you can put values that you want only and create an error message if there is another value you dont want

--- models/product.js
+++ models/product.js
@@ -17,12 +17,17 @@ const productSchema = new mongoose.Schema({
     type: Number,
     default: 4.5,
   },
-  company: {
-    type: String,
-  },
   createdAt: {
     type: Date,
     default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported", // we can create custom error message here, if there is another value we don't want to have.
+    },
+    // enum: ["ikea", "liddy", "caressa", "marcos"],
   },
 });
 
